fcb26f92a05f48a8410a3f84838f5cd5f9c0f8ce
OfficialPath <42738205+OfficialPath@users.noreply.github.com>
2021-02-28 14:51:09 -0700

    Initial Commit
    - Added Test Scene
    - Added Player Movement
    - Added Bullet Paterning

---
+++ PerfectDawn/Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private Vector3 bulletVelocity;
+    public GameObject BarrelTip;
+    public GameObject projectile;
+
+
+    public void Projectile()
+    {
+        bulletVelocity = BarrelTip.transform.position;
+        Instantiate(projectile, bulletVelocity, Quaternion.identity);
+    }
+}
